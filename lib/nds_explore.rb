f94a154c4728b75a364268085fba871b30acaf1f
Jason Nordheim <jason.nordheim@gmail.com>
2020-03-10 20:49:16 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,7 +8,7 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  directors_database[0][:movies]
+  pp directors_database[0][:movies]
 end
 
 pretty_print_nds(directors_database)
